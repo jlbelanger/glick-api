4de873881a9738811121fad4b94753c4ba3af595
Jenny Belanger <jbelangr@gmail.com>
2021-03-23 19:27:49 -0400

    Update CORS headers

--- config/cors.php
+++ config/cors.php
@@ -23,7 +23,7 @@ return [
 
 	'allowed_origins_patterns' => [],
 
-	'allowed_headers' => ['Content-Type', 'Authorization'],
+	'allowed_headers' => ['Content-Type', 'Authorization', 'X-Requested-With'],
 
 	'exposed_headers' => [],
 
