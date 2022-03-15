a8b8c0b513cc738d6948dbfd7592d999149cf551
Jenny Belanger <jbelangr@gmail.com>
2022-03-15 18:39:35 -0400

    Lint

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
 	/**
 	 * Bootstraps any application services.
 	 *
+	 * @param  Kernel $kernel
 	 * @return void
 	 */
 	public function boot(Kernel $kernel)
